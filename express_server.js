24c58a6034dd1e87606c3e882476fab5ff2c01cf
jubhavsar <juhi_bhavsar@yahoo.com>
2021-12-07 09:35:25 -0500

    Generate new URL and redirect after recieving POST request

--- express_server.js
+++ express_server.js
@@ -21,6 +21,9 @@ const urlDatabase = {
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -32,14 +35,15 @@ app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+app.post("/urls", (req, res) => {
+   const shortURL = generateRandomString();
+   urlDatabase[shortURL] = req.body.longURL;
+   //res.redirect(`/urls/${shortURL}`);
+   res.redirect(req.body.longURL);
+  });
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
